154b85b742c7d880e5965d508a07c1c29a493d03
Alex <alex.suarez@lemonade.com>
2023-11-23 09:54:21 +0100

    Refactor Commands component and TestCard component

--- src/pages/test/components/Commands.tsx
+++ src/pages/test/components/Commands.tsx
@@ -13,29 +13,12 @@ const CommandsHeader = () => {
   );
 };
 
-const DragHandle = () => {
-  return (
-    <div className="inline-flex flex-col items-center justify-center gap-1 border border-zinc-800 py-2 rounded">
-      <div className="w-1 h-1 bg-zinc-600 rounded-full"></div>
-      <div className="w-1 h-1 bg-zinc-600 rounded-full mx-2"></div>
-      <div className="w-1 h-1 bg-zinc-600 rounded-full"></div>
-    </div>
-  );
-};
-
 export const Commands = () => {
   return (
     <div className="border border-zinc-800 mt-12 rounded-lg p-5">
       <CommandsHeader />
 
-      <div className="flex flex-col">
-        {new Array(10).fill('').map((_, i) => (
-          <div key={i} className="border-b border-zinc-800 items-stretch flex">
-            <DragHandle />
-            <div className=" p-6"></div>
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-col"></div>
     </div>
   );
 };
--- src/pages/tests/components/TestCard.tsx
+++ src/pages/tests/components/TestCard.tsx
@@ -8,19 +8,19 @@ type TestCardProps = StatusBadgeProps & {
 
 export const TestCard: React.FC<TestCardProps> = ({ status, title, url }) => {
   return (
-    <div className="flex gap-3 border-b border-zinc-900 py-4">
+    <div className="flex gap-4 border-b border-zinc-900 py-4">
       <div className="w-1/3">{title}</div>
-      <div className="w-1/3 text-center">
+      <div className="w-1/3">
         <StatusBadge status={status} />
       </div>
       <div className="w-1/3">
         <div className="flex items-center gap-2">
-          <span>Commands</span>
+          <span className="text-zinc-500">Commands</span>
           <NumberBadge value={12} />
         </div>
       </div>
-      <div className="flex flex-1 justify-end">
-        <a className="text-zinc-600" target="_blank" href={url}>
+      <div className="flex flex-1 justify-end text-zinc-500 underline underline-offset-4 decoration-dashed">
+        <a target="_blank" href={url}>
           {url.replace('https://', '')}
         </a>
       </div>
