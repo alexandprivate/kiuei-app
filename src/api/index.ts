c079077a5a4e0afaf19b3f41ded7db0eb1045a0f
Alex <alex.suarez@lemonade.com>
2023-12-08 20:05:41 +0100

    Add VITE_API_URL and axios dependency

---
+++ src/api/index.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const baseUrl = import.meta.env.VITE_API_URL;
+
+export const org = {
+  read: (id: string) => axios.get(`${baseUrl}/org/${id}`).then((res) => res.data),
+  create: (data: Org) => axios.post(`${baseUrl}/org`, data).then((res) => res.data),
+  update: (id: string, data: Org) =>
+    axios.put(`${baseUrl}/org/${id}`, data).then((res) => res.data),
+  delete: (id: string) => axios.delete(`${baseUrl}/org/${id}`).then((res) => res.data)
+};
+
+export const test = {
+  read: (id: string) => axios.get(`${baseUrl}/test/${id}`).then((res) => res.data),
+  create: (data: Test) => axios.post(`${baseUrl}/test`, data).then((res) => res.data),
+  update: (id: string, data: Test) =>
+    axios.put(`${baseUrl}/test/${id}`, data).then((res) => res.data),
+  delete: (id: string) => axios.delete(`${baseUrl}/test/${id}`).then((res) => res.data)
+};
+
+export const suite = {
+  read: (id: string) => axios.get(`${baseUrl}/suite/${id}`).then((res) => res.data),
+  create: (data: Suite) => axios.post(`${baseUrl}/suite`, data).then((res) => res.data),
+  update: (id: string, data: Suite) =>
+    axios.put(`${baseUrl}/suite/${id}`, data).then((res) => res.data),
+  delete: (id: string) => axios.delete(`${baseUrl}/suite/${id}`).then((res) => res.data)
+};
+
+export const user = {
+  read: (id: string) => axios.get(`${baseUrl}/user/${id}`).then((res) => res.data),
+  create: (data: User) => axios.post(`${baseUrl}/user`, data).then((res) => res.data),
+  update: (id: string, data: User) =>
+    axios.put(`${baseUrl}/user/${id}`, data).then((res) => res.data),
+  delete: (id: string) => axios.delete(`${baseUrl}/user/${id}`).then((res) => res.data)
+};
---
+++ src/globals.d.ts
@@ -0,0 +1,32 @@
+// entities
+interface Org {
+  id: string;
+  name: string;
+  description: string;
+  created: string;
+  updated: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  created: string;
+  updated: string;
+}
+
+interface Suite {
+  id: string;
+  name: string;
+  description: string;
+  created: string;
+  updated: string;
+}
+
+interface Test {
+  id: string;
+  name: string;
+  description: string;
+  created: string;
+  updated: string;
+}
